727b0e444dd497e077db5efa06ca052ca6769510
Aballo Janet <jabalo@student.refactory.academy>
2024-03-21 11:16:55 +0300

    removed footer

---
+++ functions.js
@@ -0,0 +1,17 @@
+function greet(){
+    let age =100
+    // return "how are you"
+    return age
+}
+
+greet()
+console.log(greet())
+
+// console.log(age)
+
+function addition(a,b){
+    sum =a+b;
+    console.log(sum)
+}
+
+addition(5,7)
